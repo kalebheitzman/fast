e4abdb2faf6ade59b9f033a12ed6b4f683b19713
Kaleb Heitzman <kalebheitzman@gmail.com>
2016-01-20 03:58:55 -0500

    added firebase/php-jwt

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit7c080fd66b96f05dfc02a2179bdf9f04::getLoader();
+return ComposerAutoloaderInita33964fa327a346778948ca1f9d3b0f8::getLoader();
